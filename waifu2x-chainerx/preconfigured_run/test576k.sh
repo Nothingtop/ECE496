3719422c3dfe3ca4d2b57be2521e4b89693e6557
Hengyue Chen <hengyue.chen@mail.utoronto.ca>
2018-02-05 14:18:48 -0500

    updated test scripts to local dir

--- waifu2x-chainerx/preconfigured_run/test576k.sh
+++ waifu2x-chainerx/preconfigured_run/test576k.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python ./../waifu2x.py --gpu -1 --method noise --noise_level 0 --input /nfs/ug/thesis/thesis0/mkccgrp/test123 --arch VGG7 --output /nfs/ug/thesis/thesis0/mkccgrp/hengyue/ECE496/waifu2x-chainerx/192k_96x96 --model_dir /nfs/ug/thesis/thesis0/mkccgrp/hengyue/ECE496/waifu2x-chainerx/epoch --model_name flickrFaces_300x300_epoch192k.npz --block_size 256
+python ./../waifu2x.py --method noise --noise_level 0 --input /nfs/ug/thesis/thesis0/mkccgrp/test123 --arch VGG7 --output ../576k_96x96 --model_dir ./epoch --model_name flickrFaces_300x300_epoch576k.npz --block_size 128
 
 read Wait
