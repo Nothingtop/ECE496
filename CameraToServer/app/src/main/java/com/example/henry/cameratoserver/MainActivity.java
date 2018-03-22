2c13bcc2d09b55fc315b7d76328468851f8c4e5e
Nothingtop <henrycbj@hotmail.com>
2018-03-22 08:50:04 -0400

    app

--- CameraToServer/app/src/main/java/com/example/henry/cameratoserver/MainActivity.java
+++ CameraToServer/app/src/main/java/com/example/henry/cameratoserver/MainActivity.java
@@ -1,271 +1,185 @@
-package com.example.henry.cameratoserver;
+    package com.example.henry.cameratoserver;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.ContentUris;
-import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Environment;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.FileProvider;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.Toast;
+    import android.annotation.SuppressLint;
+    import android.app.Activity;
+    import android.content.ContentResolver;
+    import android.content.ContentUris;
+    import android.content.Context;
+    import android.content.Intent;
+    import android.database.Cursor;
+    import android.graphics.Bitmap;
+    import android.net.Uri;
+    import android.os.Build;
+    import android.os.Bundle;
+    import android.os.Environment;
+    import android.provider.DocumentsContract;
+    import android.provider.MediaStore;
+    import android.support.design.widget.FloatingActionButton;
+    import android.support.design.widget.Snackbar;
+    import android.support.v4.content.CursorLoader;
+    import android.support.v4.content.FileProvider;
+    import android.support.v7.app.AppCompatActivity;
+    import android.support.v7.widget.Toolbar;
+    import android.util.Log;
+    import android.view.View;
+    import android.view.Menu;
+    import android.view.MenuItem;
+    import android.widget.ImageView;
+    import android.widget.Toast;
 
-import com.koushikdutta.async.future.Future;
-import com.koushikdutta.async.future.FutureCallback;
-import com.koushikdutta.ion.Ion;
-import com.koushikdutta.ion.Response;
+    import com.koushikdutta.async.future.Future;
+    import com.koushikdutta.async.future.FutureCallback;
+    import com.koushikdutta.ion.Ion;
+    import com.koushikdutta.ion.Response;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+    import org.apache.commons.io.IOUtils;
+    import org.json.JSONException;
+    import org.json.JSONObject;
 
-import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
+    import java.io.File;
+    import java.io.FileNotFoundException;
+    import java.io.FileOutputStream;
+    import java.io.IOException;
+    import java.io.InputStream;
+    import java.io.OutputStream;
+    import java.net.URI;
+    import java.net.URISyntaxException;
+    import java.nio.file.FileStore;
+    import java.text.SimpleDateFormat;
+    import java.util.ArrayList;
+    import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
-    private static final int TAKE_PICTURE = 1;
-    private static final int TAKE_GALLERY = 2;
-    private Uri imageUri;
+    public class MainActivity extends AppCompatActivity {
+        static final int REQUEST_IMAGE_CAPTURE = 1;
+        private static final int TAKE_GALLERY = 2;
+        private Uri imageUri;
 
-    public class GenericFileProvider extends FileProvider {}
+        public class GenericFileProvider extends FileProvider {}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                takePhoto();
-                pickImages();
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    takePhoto();
+//                    pickImages();
+
+                }
+            });
+        }
+
+        public void takePhoto() {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
-        });
-    }
+        }
 
-    public void takePhoto() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File photo = new File(Environment.getExternalStorageDirectory(),  "Pic_Test.jpg");
-        imageUri = FileProvider.getUriForFile(this,
-                this.getApplicationContext().getPackageName() + ".provider",
-                photo);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT,
-                (FileProvider.getUriForFile(this,
-                        this.getApplicationContext().getPackageName() + ".provider",
-                        photo))).addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        startActivityForResult(intent, TAKE_PICTURE);
-    }
+        public void pickImages(){
+            Intent intent = new Intent(); intent.setType("image/*");
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+           startActivityForResult(Intent.createChooser(intent,"Select Picture"),TAKE_GALLERY);
+    //        getDialog().dismiss();
+        }
 
-    public void pickImages(){
-        Intent intent = new Intent(); intent.setType("image/*");
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-       startActivityForResult(Intent.createChooser(intent,"Select Picture"),TAKE_GALLERY);
-//        getDialog().dismiss();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case TAKE_PICTURE: {
-                if (resultCode == Activity.RESULT_OK) {
-                    Uri selectedImage = imageUri;
-                    getContentResolver().notifyChange(selectedImage, null);
-                    ImageView imageView = (ImageView) findViewById(R.id.imageView);
-//                    ContentResolver cr = getContentResolver();
-                    Bitmap bitmap;
-                    try {
-//                        bitmap = android.provider.MediaStore.Images.Media
-//                                .getBitmap(cr, selectedImage);
-
-//                        imageView.setImageBitmap(bitmap);
-                        uploadImage(selectedImage);
-                        Toast.makeText(this, selectedImage.toString(),
-                                Toast.LENGTH_LONG).show();
-                    } catch (Exception e) {
-                        Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
-                                .show();
-                        Log.e("Camera", e.toString());
-                    }
-                }
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
             }
-            case TAKE_GALLERY: {
-                if (resultCode == RESULT_OK) {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && data != null) {
+
+            return super.onOptionsItemSelected(item);
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            switch (requestCode) {
+                case REQUEST_IMAGE_CAPTURE: {
+                    if (resultCode == Activity.RESULT_OK) {
+                        Bundle extras = data.getExtras();
+                        Bitmap imageBitmap = (Bitmap) extras.get("data");
                         try {
-                            Uri uri = data.getData();
-                            uploadImage(new File(getTruePath(uri)));
+                            uploadImage(createImageFile(imageBitmap));
                         } catch (Exception e) {
-                            Log.e("Pick Image Failed With:", e.getMessage());
+                            Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
+                                    .show();
+                            Log.e("Camera", e.toString());
                         }
                     }
-                } else if (resultCode == RESULT_CANCELED) {
-                    // The user canceled the operation.
+                }
+                case TAKE_GALLERY: {
+                    if (resultCode == RESULT_OK) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && data != null) {
+                            try {
+                                Uri uri = data.getData();
+//                                uploadImage(uri);
+                            } catch (Exception e) {
+                                Log.e("Pick Image Failed With:", e.getMessage());
+                            }
+                        }
+                    } else if (resultCode == RESULT_CANCELED) {
+                        // The user canceled the operation.
+                    }
                 }
             }
         }
-    }
 
-    @SuppressLint("NewApi")
-    public String getFilePath(Uri uri) throws URISyntaxException {
-        String selection = null;
-        String[] selectionArgs = null;
-        // Uri is different in versions after KITKAT (Android 4.4), we need to
-        if (Build.VERSION.SDK_INT >= 19 && DocumentsContract.isDocumentUri(this, uri)) {
-            if (isExternalStorageDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                return Environment.getExternalStorageDirectory() + "/" + split[1];
-            } else if (isDownloadsDocument(uri)) {
-                final String id = DocumentsContract.getDocumentId(uri);
-                uri = ContentUris.withAppendedId(
-                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
-            } else if (isMediaDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-                if ("image".equals(type)) {
-                    uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                } else if ("video".equals(type)) {
-                    uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                } else if ("audio".equals(type)) {
-                    uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                }
-                selection = "_id=?";
-                selectionArgs = new String[]{
-                        split[1]
-                };
-            }
+        private File createImageFile(Bitmap bitmap) throws IOException {
+            // Create an image file name
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String imageFileName = "JPEG_" + timeStamp + "_";
+            File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+            File image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+
+            OutputStream outputStream = new FileOutputStream(image);
+
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+            // Save a file: path for use with ACTION_VIEW intents
+            return image;
         }
-        if ("content".equalsIgnoreCase(uri.getScheme())) {
-            String[] projection = {
-                    MediaStore.Images.Media.DATA
-            };
-            Cursor cursor = null;
-            try {
-                cursor = this.getContentResolver()
-                        .query(uri, projection, selection, selectionArgs, null);
-                return cursor.getString(0);
-//                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-//                if (cursor.moveToFirst()) {
-//                    return cursor.getString(column_index);
-//                }
-            } catch (Exception e) {
-            }
-        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
-            return uri.getPath();
-        }
-        return null;
-    }
 
-    public static boolean isExternalStorageDocument(Uri uri) {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-    public static boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-    public static boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
-    }
-
-    private void uploadImage(File f) {
+        private void uploadImage(File im) {
             Future uploading = Ion.with(MainActivity.this)
-                    .load("http://192.168.137.1:44000/upload")
-                    .setMultipartFile("image", f)
-                    .asString()
-                    .withResponse()
-                    .setCallback(new FutureCallback<Response<String>>() {
-                        @Override
-                        public void onCompleted(Exception e, Response<String> result) {
-                            try {
-                                JSONObject jobj = new JSONObject(result.getResult());
-                                Toast.makeText(getApplicationContext(), jobj.getString("response"), Toast.LENGTH_SHORT).show();
+                        .load("http://192.168.33.131:44000/upload")
+                        .setMultipartFile("image", im)
+                        .asString()
+                        .withResponse()
+                        .setCallback(new FutureCallback<Response<String>>() {
+                            @Override
+                            public void onCompleted(Exception e, Response<String> result) {
+                                try {
+                                    JSONObject jobj = new JSONObject(result.getResult());
+                                    Toast.makeText(getApplicationContext(), jobj.getString("response"), Toast.LENGTH_SHORT).show();
+                                } catch (JSONException e1) {
+                                    System.out.println(e1.getMessage());
+                                }
 
-                            } catch (JSONException e1) {
-                                e1.printStackTrace();
                             }
-
-                        }
-                    });
-    }
-
-    public String getTruePath(Uri uri) {
-        String[] projection = { MediaStore.Images.Media.DATA };
-        Cursor cursor = managedQuery(uri, projection, null, null, null);
-        startManagingCursor(cursor);
-        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        cursor.moveToFirst();
-        return cursor.getString(column_index);
-    }
-
-    private void uploadImage(Uri contentUri) {
-        try {
-            Future uploading = Ion.with(MainActivity.this)
-                    .load("http://192.168.137.1:44000/upload")
-                    .setMultipartFile("image", new File(getFilePath(contentUri)))
-                    .asString()
-                    .withResponse()
-                    .setCallback(new FutureCallback<Response<String>>() {
-                        @Override
-                        public void onCompleted(Exception e, Response<String> result) {
-                            try {
-                                JSONObject jobj = new JSONObject(result.getResult());
-                                Toast.makeText(getApplicationContext(), jobj.getString("response"), Toast.LENGTH_SHORT).show();
-
-                            } catch (JSONException e1) {
-                                e1.printStackTrace();
-                            }
-
-                        }
-                    });
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
+                        });
         }
     }
-}
